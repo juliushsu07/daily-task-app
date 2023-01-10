721cd154c513ee5401079340903ec0ffdae67407
Julius Hsu <32544328+juliushsu07@users.noreply.github.com>
2023-01-10 17:56:53 -0500

    Create useEffect hook to fetch tasks from backend to frontend.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { React, useEffect, useState } from 'react'
 
 function Home() {
+  const [tasks, setTasks] = useState(null)
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const res = await fetch('/api/tasks')
+      const json = await res.json()
+
+      if(res.ok) {
+        setTasks(json)
+      }
+    }
+
+    fetchWorkouts()
+  }, [])
+
+
   return (
     <div className="home">
-      <h2>Home</h2>
+      <div className = "tasks">
+        {tasks && tasks.map(task => (
+            <p key={task._id}>{task.title}</p>
+        ))}
+      </div>
     </div>
   )
 }
