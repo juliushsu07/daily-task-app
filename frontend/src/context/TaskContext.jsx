beb572163a30f6ad7ab91294d66a0e934f98af74
Julius Hsu <32544328+juliushsu07@users.noreply.github.com>
2023-01-10 20:28:08 -0500

    Create TaskContext and useTaskContext Hook.

---
+++ frontend/src/context/TaskContext.jsx
@@ -0,0 +1,30 @@
+import { createContext, useReducer} from 'react'
+
+export const TaskContext = createContext()
+
+export const taskReducer = (state, action) => {
+    switch (action.type) {
+        case 'SET_TASKS': 
+            return {
+                tasks: action.payload
+            }
+        case 'CREATE_TASK':
+            return {
+                task: [action.payload, ...state.tasks]
+            }
+        default: 
+        return state
+    }
+}
+
+export const TaskConetextProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(taskReducer, {
+        tasks: null
+    })
+
+    return (
+        <TaskContext.Provider value={{state, dispatch}}>
+            { children }
+        </TaskContext.Provider>
+      )
+}
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { TaskConetextProvider } from './context/TaskContext'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <TaskConetextProvider>
+      <App />
+    </TaskConetextProvider>
   </React.StrictMode>
 );
