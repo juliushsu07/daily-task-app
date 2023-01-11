fa8acc17a3e4b9c397427b613919647608d9f22d
Julius Hsu <32544328+juliushsu07@users.noreply.github.com>
2023-01-10 21:20:36 -0500

    Create Delete button and implemnt it's functionality.

--- frontend/src/components/TaskDetails.jsx
+++ frontend/src/components/TaskDetails.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import { useTaskContext } from '../hooks/useTaskContext'
 
-const TaskDetails = ({task}) => {
+const TaskDetails = ({ task }) => {
+  const { dispatch } = useTaskContext()
+
+  const handleClick = async () => {
+    const response = await fetch('/api/tasks/' + task._id, {
+      method: 'DELETE'
+    })
+    const json = await response.json()
+
+    if (response.ok) {
+      dispatch({type: 'DELETE_TASK', payload: json})
+    }
+  }
+
   return (
     <div className="task-details">
       <h4>{task.title}</h4>
       <p>{task.createdAt}</p>
+      <span onClick={handleClick}>Done!</span>
     </div>
   )
 }
